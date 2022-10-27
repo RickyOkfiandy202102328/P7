80f7e382a6df2374fecba999631c6ec7be047e7e
Richie Okfiandy <asungahie@gmail.com>
2022-10-27 21:23:49 +0700

    first

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { Dashboard3Component } from './dashboard3/dashboard3.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminComponent } from './admin/admin.component';
+
+const routes: Routes = [
+  {path: "dashboard", component: DashboardComponent},
+  {path: "login", component: LoginComponent},
+  {path: "register", component: RegisterComponent},
+  {path: "admin", component: AdminComponent},
+  {path: "dashboard2", component: Dashboard2Component},
+  {path: "dashboard3", component: Dashboard3Component},
+  {path: "", redirectTo: "login", pathMatch: "full"},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, AfterViewInit, Renderer2 } from '@angular/core';
+
+declare const $:any;
+declare const Plugin:any;
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit, AfterViewInit {
+
+  constructor(private renderer : Renderer2) { }
+  ngAfterViewInit(): void {
+    this.renderer.removeClass(document.body,"login-page");
+    this.renderer.removeClass(document.body,"sidebar-open");
+    this.renderer.addClass(document.body,"sidebar-closed");
+
+    $('data-widget="treeview"').Treeview("init");
+    Plugin.call($(this))
+  }
+  ngOnInit(): void {
+  }
+
+}
